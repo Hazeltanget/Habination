bdda7ba95f7fa466889ba0d8bef58e6f06a97a00
Bolshachkov Denis <programmerHazelTanget@gmail.com>
2022-04-05 15:50:22 +0300

    Initial commit

---
+++ HabbiTracker/HabbiTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabbiTrackerApp.swift
+//  HabbiTracker
+//
+//  Created by Денис Большачков on 05.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct HabbiTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+<h1>HabbiTracker</h1>
+
+<p>Developer: Bolshachkov Denis</p>
+<br/>
+<p>Designer: Averianov Ivan</p>
